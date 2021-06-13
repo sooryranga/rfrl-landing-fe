a3a2489b8da433d51431680e527d620df6ffb5a9
Arun Ranganathan <arun.ranga@hotmail.ca>
2021-06-13 15:16:29 -0400

    feat: get main page working

---
+++ src/components/Main.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="scrolable-full">
+    <div :style="mainContainer">
+      <div class="h-100 container-md">
+        <div class="h-100 w-100 flex-container">
+          <div id="headline" class="my-auto p-5 white-overlay text-container">
+            <h1 id="title"><span class="logo">rfrl</span> helps professionals land their dream job</h1>
+            <p id="subtitle">Get referred and improve your chances by 20 folds</p>
+            <button type="button" class="btn btn-lg btn-light" id="cta">Get Your Dream Job</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-100 h-100" :style="mainContainer">
+      <div class="h-100 container-md">
+        <div class="h-100 w-100 flex-container">
+          <div id="headline" class="my-auto p-5 white-overlay text-container">
+            <h1 id="title"><span class="logo">rfrl</span> helps professionals land their dream job</h1>
+            <p id="subtitle">Get referred and improve your chances by 20 folds</p>
+            <button type="button" class="btn btn-lg btn-light" id="cta">Get Your Dream Job</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      mainContainer: {
+        background: `url(${
+          require('@/assets/pexels-christina-morillo-1181275.jpg')
+        }) no-repeat center center`,
+        backgroundSize: 'cover',
+        boxShadow: 'inset 2000px 0 0 0 rgba(0, 0, 0, 0.5)',
+        position: 'relative',
+        height: '100%',
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.scrolable-full{
+  height: 100%;
+  width: 100%;
+  overflow-y: scroll;
+}
+
+.white-overlay{
+  background-color: rgba(255, 255, 255, 0.85);
+}
+
+.flex-container{
+  display:flex;
+}
+
+.text-container{
+  max-width: 70ch;
+}
+
+#headline{
+  color: var(--clr-gray-4);
+  margin-left:1rem;
+  margin-right:1rem;
+  min-width: 0px;
+  min-height:0px;
+}
+
+#cta{
+  font-size:1.6rem;
+  font-weight: 500;
+  color: var(--clr-gray-1);
+}
+
+#title{
+  letter-spacing: -1px;
+}
+
+#subtitle{
+  letter-spacing: 1px;
+  margin: 1rem 1rem 0 0;
+}
+
+.logo{
+  font-family: 'Orbitron', sans-serif;
+  color: var(--clr-gray-2);
+  font-size: 4rem;
+}
+</style>
---
+++ src/components/NavLink.vue
@@ -0,0 +1,90 @@
+<template>
+  <a
+    :href="href"
+    @click="navigate"
+    class="nav-item h-100"
+    @mouseleave="hover = false"
+    @mouseover="hover = true">
+    <div class="link-container">
+      <div class="name"><slot></slot></div>
+      <transition name="slide-fade">
+        <div v-if="show" class="line"/>
+      </transition>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+    href: {
+      type: String,
+      required: true,
+    },
+    navigate: {
+      type: Function,
+      required: true,
+    },
+  },
+  computed: {
+    show() {
+      return this.active || this.hover;
+    },
+  },
+  data() {
+    return {
+      hover: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.nav-item {
+  vertical-align: middle;
+}
+
+.link-container {
+  height: 100%
+}
+
+.name {
+  position: relative;
+  top: calc(50% - 0.7rem);
+}
+
+a {
+  font-size: 1em;
+  font-weight: 600;
+  color: white;
+  letter-spacing: 1px;
+  text-decoration: none;
+}
+
+.line {
+  height: 2px;
+  width: 100%;
+  position: relative;
+  top: 0.8rem;
+  background-color: white;
+  margin-top:0.2rem;
+}
+
+
+.slide-fade-enter-active {
+  transition: all .3s cubic-bezier(0.22, 0.61, 0.36, 1);
+}
+.slide-fade-leave-active {
+  transition: all .3s cubic-bezier(0.22, 0.61, 0.36, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
