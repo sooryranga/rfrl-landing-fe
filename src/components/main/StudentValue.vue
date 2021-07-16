b77b6c006e78408664f1997c0f81deb3aaf65874
Soory Ranganathan <soory.ranga@hotmail.com>
2021-07-15 20:22:09 -0400

    feat: work on small screen

--- src/components/main/StudentValue.vue
+++ src/components/main/StudentValue.vue
@@ -1,66 +1,36 @@
 <template>
   <div class="w-100" id="section">
     <h2 id="section-title">Want your dream job?</h2>
-    <div class="row mt-4 mb-4">
-      <div class="col">
-        <div class="subsection flex-container">
-          <div class="my-auto">
-            <img src="@/assets/pexels-freestocksorg-122477.jpg" class="feature-img"/>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="subsection flex-container">
-          <div class="my-auto">
-            <h3 class="subsection-title">Skip the line</h3>
-            <p class="subsection-description">
-              Typical job posting for FANG companies get over 500 applicants.
-              How will you standout out of 100s of engineers? You can, by getting referred by
-              veteran from the company.
-            </p>
-          </div>
-        </div>
+    <div class="mt-4 mb-4 subsection">
+      <img src="@/assets/pexels-freestocksorg-122477.jpg" class="feature-img"/>
+      <div class="my-auto right-text feature-text">
+        <h3 class="subsection-title">Skip the line</h3>
+        <p>
+          Typical job posting for FANG companies get over 500 applicants.
+          How will you standout out of 100s of engineers? You can, by getting referred by
+          veteran from the company.
+        </p>
       </div>
     </div>
-    <div class="row mb-4">
-      <div class="col">
-        <div class="subsection flex-container flex-end">
-          <div class="my-auto" style="text-align: right;">
-            <h3 class="subsection-title">Review your skills</h3>
-            <p class="subsection-description">
-              You need to pass the interview to get the job. What better way to
-              practice interviewing than with the person who passed the interview.
-            </p>
-          </div>
-        </div>
+    <div class="subsection mb-4">
+      <div class="my-auto left-text feature-text">
+        <h3 class="subsection-title">Review your skills</h3>
+        <p>
+          You need to pass the interview to get the job. What better way to
+          practice interviewing than with the person who passed the interview.
+        </p>
       </div>
-      <div class="col">
-        <div class="subsection flex-container">
-          <div class="my-auto">
-            <img src="@/assets/pexels-fauxels-3184460.jpg" class="feature-img"/>
-          </div>
-        </div>
-      </div>
+      <img src="@/assets/pexels-fauxels-3184460.jpg" class="feature-img"/>
     </div>
-    <div class="row">
-      <div class="col">
-        <div class="subsection flex-container">
-          <div class="my-auto">
-            <img src="@/assets/pexels-ketut-subiyanto-4560150.jpg" class="feature-img"/>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="subsection flex-container">
-          <div class="my-auto">
-            <h3 class="subsection-title">Stack your resume</h3>
-            <p class="subsection-description">
-              Within the first two session, referrer would let you know whether you would be qualified for referral.
-              If you aren't, we can help stack your resume by working together on a open source project together
-              setting you up for success next time around.
-            </p>
-          </div>
-        </div>
+    <div class="subsection mb-4">
+      <img src="@/assets/pexels-ketut-subiyanto-4560150.jpg" class="feature-img"/>
+      <div class="my-auto right-text feature-text">
+        <h3 class="subsection-title">Stack your resume</h3>
+        <p>
+          Within the first two session, referrer would let you know whether you would be qualified for referral.
+          If you aren't, we can help stack your resume by working together on a open source project together
+          setting you up for success next time around.
+        </p>
       </div>
     </div>
   </div>
@@ -86,20 +56,65 @@ export default {
   color: var(--clr-gray-3);
 }
 
-.subsection-description{
-  max-width: 50ch;
-}
-
 .subsection{
-  min-height: 20rem;
+  max-width: 100%;
+  height: 30rem;
+  display:flex;
+  flex-flow: row nowrap;
+  justify-content: center;
 }
 
 .feature-img{
-  width: 100%;
+  max-width: 50%;
+  object-fit: cover;
 }
 
-.flex-end{
-  flex-flow: row nowrap;
-  justify-content:flex-end;
+.feature-text{
+  max-width: 25rem;
+  width: 50%;
+}
+
+.right-text{
+  padding-left: 1rem;
+  text-align: left;
+}
+
+.left-text{
+  padding-right: 1rem;
+  text-align: right;
+}
+
+
+@media (max-width: 800px) {
+  .feature-img{
+    max-height: 70%;
+    max-width: 100%;
+    object-fit: cover;
+  }
+
+  .subsection{
+    max-width: 100%;
+    height: 35rem;
+    display:flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .left-text{
+    padding-right: 0;
+    text-align: center;
+    order: 5;
+  }
+
+  .right-text{
+    padding-left: 0;
+    text-align: center;
+  }
+
+  .feature-text{
+    max-width: 25rem;
+    width: 75%;
+  }
 }
 </style>
